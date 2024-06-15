805f5f19a4b041657bf6aa2b3a278a33ecf34a90
deepshamenghani <46545400+deepshamenghani@users.noreply.github.com>
2024-06-14 22:00:46 -0700

    gitignore

--- conversationchatbot_fromenv.py
+++ conversationchatbot_fromenv.py
@@ -1,21 +1,16 @@
-# Install packages if needed
-# pip install openai
-# pip install python-dotenv
-
 from openai import OpenAI
 import os
 import dotenv
 
 dotenv.load_dotenv()
-# Load your API key from an environment variable
 client = OpenAI(api_key=os.getenv('OPENAI_API'))
 
-# Create an instruction for our bot
+# Create an instruction for the bot
 conversation = [{"role": "system", "content": "You are an assistant."}]
 
 def ask_question(inputquestion):
     # Append the user's question to the conversation array
-    conversation.append({"role": "system","content": inputquestion})
+    conversation.append({"role": "user","content": inputquestion})
 
     # Request gpt-3.5-turbo for chat completion or a response
     response = client.chat.completions.create(
@@ -25,5 +20,9 @@ def ask_question(inputquestion):
 
     # Access the response from the API to display it
     assistant_response = response.choices[0].message.content
-
+    
     return assistant_response
+
+# ask_question("What is customer churn? Respond in less than 10 words")
+
+
